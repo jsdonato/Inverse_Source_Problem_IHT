a964583310472a60250a78d79f1fa724af06fc1d
jsd982412 <jsd982412@gmail.com>
2020-03-17 11:36:32 -0400

    Added some functionalitites to the matrix class and added more to the circle custom constructor for the lattice

--- isp.cpp
+++ isp.cpp
@@ -9,6 +9,9 @@
 using namespace std;
 using namespace arma;
 
+const double freq = 1;
+const double med_coeff = 1;
+
 class Point{
 public:
     Point(double x_n, double y_n, double z_n, double eta_n){
@@ -32,6 +35,10 @@ public:
     
     double Eta(){
         return eta;
+    }
+    
+    void set_Eta(double eta_n){
+        eta = eta_n;
     }
     
 private:
@@ -76,13 +83,28 @@ public:
         for (size_t i = 0; i < (size_n + 1); i++){
             for (size_t j = 0; j < (size_n + 1); j++){
                 for (size_t k = 0; k < (size_n + 1); k++){
-                    lattice_temp.emplace_back(k / size_n, j / size_n ,i / size_n, 0);
-                    /*if (){
+                    
+                    double x = (double)k / (double)size_n;
+                    double y = (double)j / (double)size_n;
+                    double z = (double)i / (double)size_n;
+                    
+                    lattice_temp.emplace_back(x, y , z, 0);
+                    
+                    if (((x-x_0)*(x-x_0)) + ((y-y_0)*(y-y_0)) + ((z-z_0)*(z-z_0)) <= radius * radius){
                         
+                        lattice_temp[lattice_temp.size() - 1].set_Eta(med_coeff);
+                    
                     }
-                    else{
-                        
-                    }*/
+                }
+            }
+        }
+        
+        for (size_t i = 0; i < size_n; i++){
+            for (size_t j = 0; j < size_n; j++){
+                for (size_t k = 0; k < size_n; k++){
+                    //In this loop I will take the averages of eta we have in lattice_temp as we did in
+                    //make_gridn in the previous routine
+                    
                 }
             }
         }
@@ -95,6 +117,37 @@ private:
 
 class Matrix{
 public:
+    Matrix(Lattice &l){
+        
+    }
+    
+    complex<double> Ai(double rx, double ry, double rz, double theta, double phi){
+        complex<double> num(0.0, freq * ((rx * cos(theta) * sin(phi)) + (ry * sin(theta) * sin(phi)) + (rz * cos(phi))));
+        return exp(num);
+    }
+    
+    complex<double> G(double rx, double ry, double rz, double rxs, double rys, double rzs){
+        double mag = sqrt(pow(rx - rxs, 2) + pow(ry - rys, 2) + pow(rz - rzs, 2));
+        double num = (1 / (4 * M_PI * mag));
+        complex<double> numtop(0.0, freq * mag);
+        return num * exp(numtop);
+    }
+    
+    cx_mat Gv(){
+        
+    }
+    
+    cx_mat V(){
+        
+    }
+    
+    cx_mat Gd(){
+        
+    }
+    
+    cx_mat Gs(){
+        
+    }
 
 private:
     
