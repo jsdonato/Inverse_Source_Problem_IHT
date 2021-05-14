e84f3dd436572e147d8cd512c0791603f1a834b6
Joe Donato <37385419+jsdonato@users.noreply.github.com>
2021-05-13 22:28:48 -0400

    Update lattice.hpp

--- lattice.hpp
+++ lattice.hpp
@@ -1,11 +1,3 @@
-//
-//  lattice.hpp
-//  ISP3C
-//
-//  Created by Joseph Donato on 4/5/20.
-//  Copyright © 2020 Joseph Donato. All rights reserved.
-//
-
 #ifndef lattice_hpp
 #define lattice_hpp
 
