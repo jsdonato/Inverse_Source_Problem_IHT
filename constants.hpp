b4bebce1a8d7d58a85e34a35a7d48406d636b4b8
Joe Donato <37385419+jsdonato@users.noreply.github.com>
2021-08-28 11:00:39 -0400

    Update constants.hpp
    Added descriptions to constants.hpp so the user has a better understanding of the parameters of the problem.

--- constants.hpp
+++ constants.hpp
@@ -1,15 +1,15 @@
 #ifndef constants_hpp
 #define constants_hpp
 
-const double freq = 1.0;
+const double freq = 1.0; //Frequency of incoming sources.
 
-const int num_div = 10;
+const int num_div = 10; //The test sources in theta x phi are placed in a equally spaced num_div x num_div grid. 
 
 //lattice
 
-const int len = 10;
+const int len = 10; //As the medium is discretized, len represents the length of the cubes making up the discretized mesh.
 
-const double eta = 0;
+const double eta = 0; //eta (>0) is a measure of how much the medium scatters light with higher magnitude translating to more scattering. 
 
 const double dL = 1.0 / (double)len;
 
