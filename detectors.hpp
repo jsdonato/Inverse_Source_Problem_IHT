1060079da9166048eaf26b9351d1db9be5f55876
Joe Donato <37385419+jsdonato@users.noreply.github.com>
2021-08-30 21:09:26 -0400

    Update detectors.hpp
    Added new constructor and descriptions

--- detectors.hpp
+++ detectors.hpp
@@ -4,19 +4,35 @@
 #include <stdio.h>
 #include <vector>
 #include "point.hpp"
+#include "constants.hpp"
 
 class Detectors{
 public:
-    //This constructor constructs a sphere of 800 evenly spaced detectors around the medium centered at (x_0, y_0, z_0)
+    //Default constructor
+    Detectors() : detectors({}) {}
+    
+    //This constructor constructs a sphere of evenly spaced detectors around the medium centered at (x_0, y_0, z_0)
     Detectors(double x_0, double y_0, double z_0, double radius){
-        int num1 = 40;
-        int num2 = 20;
-        detectors.reserve(num1 * num2);
-        for (int i = 0; i < num1; i++){
-            for (int j = 0; j < num2; j++){
-                double x = (radius * cos((i * 2 * M_PI) / num1) * sin((j * M_PI) / num2)) + x_0;
-                double y = (radius * sin((i * 2 * M_PI) / num1) * sin((j * M_PI) / num2)) + y_0;
-                double z = (radius * cos((j * M_PI) / num2)) + z_0;
+        detectors.reserve(2 * detect_div_pi * detect_div_pi);
+        for (int i = 0; i < 2 * detect_div_pi; i++){
+            for (int j = 0; j < detect_div_pi; j++){
+                double x = (radius * cos((i * 2 * M_PI) / (2 * detect_div_pi)) * sin((j * M_PI) / detect_div_pi)) + x_0;
+                double y = (radius * sin((i * 2 * M_PI) / (2 * detect_div_pi)) * sin((j * M_PI) / detect_div_pi)) + y_0;
+                double z = (radius * cos((j * M_PI) / detect_div_pi)) + z_0;
+                detectors.emplace_back(x, y, z, 0.0);
+            }
+        }
+    }
+
+    //This constructor constructs a sphere of evenly spaced detectors around the medium centered at (vec[0], vec[1], vec[2])
+    //and radius vec[3]
+    Detectors(std::vector<double> vec){
+        detectors.reserve(2 * detect_div_pi *  detect_div_pi);
+        for (int i = 0; i < 2 * detect_div_pi; i++){
+            for (int j = 0; j < detect_div_pi; j++){
+                double x = (vec[3] * cos((i * 2 * M_PI) / (2 * detect_div_pi)) * sin((j * M_PI) / detect_div_pi)) + vec[0];
+                double y = (vec[3] * sin((i * 2 * M_PI) / (2 * detect_div_pi)) * sin((j * M_PI) / detect_div_pi)) + vec[1];
+                double z = (vec[3] * cos((j * M_PI) / detect_div_pi)) + vec[2];
                 detectors.emplace_back(x, y, z, 0.0);
             }
         }
