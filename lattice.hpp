9e7379f4040a15f8ba6b3ff6ff88f7fc930fae1b
Joe Donato <37385419+jsdonato@users.noreply.github.com>
2021-08-30 21:11:50 -0400

    Update lattice.hpp
    Updated Lattice class with polymorphism and descriptions of constructors.

--- lattice.hpp
+++ lattice.hpp
@@ -11,29 +11,82 @@
 
 class Lattice{
 public:
+    Lattice() : lattice({}) {}
+    
+    virtual void make_cell(size_t i, size_t j, size_t k) = 0;
+    
+    
+    Point at(size_t i) const {
+        return lattice[i];
+    }
+    
+    size_t Size() const {
+        return lattice.size();
+    }
+    
+    virtual ~Lattice() = default;
+    
+protected:
+    std::vector<Point> lattice;
+};
+
+class UniformLattice : public Lattice {
+public:
     //This constructor makes a lattice with uniform eta throughout.
-    Lattice(){
+    UniformLattice(){
         for (size_t i = 0; i < len; i++){
             for (size_t j = 0; j < len; j++){
                 for (size_t k = 0; k < len; k++){
-                    make_cell_uni(i, j, k);
+                    make_cell(i, j, k);
                 }
             }
         }
     }
-    
+
+    void make_cell(size_t i, size_t j, size_t k){
+        double x = (double)((2 * k) + 1)/(double)(2 * len);
+        double y = (double)((2 * j) + 1)/(double)(2 * len);
+        double z = (double)((2 * i) + 1)/(double)(2 * len);
+        
+        Point temp(x, y, z, eta);
+        lattice.push_back(temp);
+    }
+
+};
+
+class SphericalLattice : public Lattice {
+private:
+    double x_0, y_0, z_0, radius;
+
+public:
     //This constructor makes a lattice with a spherical region filled set to eta and the rest set to zero.
-    Lattice(double x_0, double y_0, double z_0, double radius){
+    //This sphere is defined with radius <radius> and center at (x_0,y_0,z_0).
+    SphericalLattice(double x0, double y0, double z0, double rad)
+        : x_0(x0), y_0(y0), z_0(z0), radius(rad) {
         for (size_t i = 0; i < len; i++){
             for (size_t j = 0; j < len; j++){
                 for (size_t k = 0; k < len; k++){
-                    make_cell(i, j, k, x_0, y_0, z_0, radius);
+                    make_cell(i, j, k);
                 }
             }
         }
     }
-    
-    void make_cell(size_t i, size_t j, size_t k, double x_0, double y_0, double z_0, double radius){
+
+
+    //This constructor makes a lattice with a spherical region filled set to eta and the rest set to zero.
+    //This sphere is defined with radius vec[3] and center at (vec[0],vec[1],vec[2]).
+    SphericalLattice(std::vector<double> vec)
+        : x_0(vec[0]), y_0(vec[1]), z_0(vec[2]), radius(vec[3]) {
+        for (size_t i = 0; i < len; i++){
+            for (size_t j = 0; j < len; j++){
+                for (size_t k = 0; k < len; k++){
+                    make_cell(i, j, k);
+                }
+            }
+        }
+    }
+
+    void make_cell(size_t i, size_t j, size_t k){
         double x = (double)((2 * k) + 1)/(double)(2 * len);
         double y = (double)((2 * j) + 1)/(double)(2 * len);
         double z = (double)((2 * i) + 1)/(double)(2 * len);
@@ -57,26 +110,7 @@ public:
         temp.set_Eta((double)sum / 8.0);
         lattice.push_back(temp);
     }
-    
-    void make_cell_uni(size_t i, size_t j, size_t k){
-        double x = (double)((2 * k) + 1)/(double)(2 * len);
-        double y = (double)((2 * j) + 1)/(double)(2 * len);
-        double z = (double)((2 * i) + 1)/(double)(2 * len);
-        
-        Point temp(x, y, z, eta);
-        lattice.push_back(temp);
-    }
-    
-    Point at(size_t i) const {
-        return lattice[i];
-    }
-    
-    size_t Size() const {
-        return lattice.size();
-    }
-    
-private:
-    std::vector<Point> lattice;
+
 };
 
 #endif /* lattice_hpp */
